7f0892dc0e3b843c72b3784992aef9cf5d46d06b
Annia Sebold <anniasebold3.0@gmail.com>
2020-05-10 23:22:47 -0400

    validations on length of post and name category

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,4 +3,5 @@ class Category < ApplicationRecord
   has_many :posts, through: :post_categories
 
   validates_presence_of :name
+  validates :name, length: { minimum: 5, maximum: 100 }
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,9 +4,10 @@ class Post < ApplicationRecord
   has_many :categories, through: :post_categories
 
   validates_presence_of :title, :text, :categories
+  validates :title, length: { minimum: 5, maximum: 255 }
   
   scope :ordered_by_created_at, -> { 
     order(created_at: :desc) 
   }
-  
+
 end
