31c2d22426f33f54eeb787e6b84c22d9f8f57e4a
Annia Sebold <anniasebold3.0@gmail.com>
2020-05-14 23:30:56 -0400

    edit posts

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
 
-  resources :posts, only: [:new ,:create, :index, :show]
+  resources :posts, only: [:new ,:create, :index, :show, :edit, :update, :destroy]
 end
