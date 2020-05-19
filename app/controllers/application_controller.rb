0dbb3e0b80fbefcb00aeae9ca5f4cd4ef0f18ba2
Annia Sebold <anniasebold3.0@gmail.com>
2020-05-19 14:56:31 -0400

    final ajusts image post and initial ajusts in avatar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :education])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :education])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :education, :avatar])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :education, :avatar])
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   extend Enumerize
   
+  has_one_attached :avatar
   has_many :posts
   validates_presence_of :email, :name, :education, :password
   
